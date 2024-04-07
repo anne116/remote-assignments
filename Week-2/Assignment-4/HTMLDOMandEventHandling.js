061f86a5be5878cef92ab13c8123f6ad15c0aa39
anne116 <130838732+anne116@users.noreply.github.com>
2024-04-07 11:52:40 +0800

    Add files via upload

--- Week-2/Assignment-4/HTMLDOMandEventHandling.js
+++ Week-2/Assignment-4/HTMLDOMandEventHandling.js
@@ -1,1 +1,31 @@
+/*
+Assignment 4: HTML DOM and Event Handling
+Following the assignment in week 1, let’s add some effects on it by only pure JavaScript without Bootstrap, jQuery, or any other libraries.
 
+Request 1: Click to Change Text.
+When the user clicks on the "Welcome Message" block, change the text to "Have a Good
+Time!".
+
+Request 2: Click to Show More Content Boxes.
+There are some more content boxes waiting to show. When the user clicks the Call-to-Action button, show those hidden content boxes.
+Hint: all content boxes are already there, they are just set to display: none at the beginning.
+*/
+
+const headline = document.getElementById('welcomeMsg');
+headline.addEventListener('click', () => {
+    headline.textContent = "Have a Good Day!";
+});
+
+
+const ctabutton = document.querySelector('.cta');
+
+ctabutton.addEventListener('click', () => {
+    const container2 = document.querySelector('.container2');
+
+    if (container2.style.display === 'none') {
+        container2.style.display = 'flex';
+    } else {
+        container2.style.display = 'none';
+    }
+    
+});
