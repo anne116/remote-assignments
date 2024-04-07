0fd3875e34b65cb351da5bb0b03d86242794539a
anne116 <130838732+anne116@users.noreply.github.com>
2024-04-07 11:52:59 +0800

    Add files via upload

--- Week-2/Assignment-1/FunctionAndArray.js
+++ Week-2/Assignment-1/FunctionAndArray.js
@@ -1,1 +1,42 @@
+/*
+Assignment 1: Function and Array
+1. max: find the max value of an array of numbers
+2. findPosition: find the first position of the target number inside an array of numbers.
+The position should be counted starting from 0, if you can't find the target, please
+return -1
 
+Reminder: you cannot use those built-in functions like Math.max() and
+Array.prototype.findIndex() to complete this assignment, please implement it by yourself.
+*/
+
+function max(numbers) {
+// your code here, for-loop method preferred
+    let maxNumber = numbers[0];
+    for ( i = 0; i < numbers.length; i++ ) {
+
+        if (numbers[i] > maxNumber ) {
+            maxNumber = numbers[i];
+        }
+    }
+    return maxNumber;
+}
+
+// console.log(max([1, 2, 4, 5])); // should print 5
+// console.log(max([5, 2, 7, 1, 6])); // should print 7
+
+function findPosition(numbers, target) {
+// your code here, for-loop method preferred
+    let position = -1;
+    for ( let i = 0; i< numbers.length; i++ ) {
+        if ( numbers[i] === target ) {
+            position = i;
+            break;
+        } 
+    }
+    return position;
+}
+
+// console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0
+// console.log(findPosition([5, 2, 7, 1, 6], 7)); // should print 2
+// console.log(findPosition([5, 2, 7, 7, 7, 1, 6], 7)); // should print 2 (the first position)
+// console.log(findPosition([5, 2, 7, 1, 6], 8)); // should print -1
