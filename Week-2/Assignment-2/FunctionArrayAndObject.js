500504540727ff0d2cf5f560f62de0fc7878f6a1
anne116 <130838732+anne116@users.noreply.github.com>
2024-04-07 11:53:34 +0800

    Add files via upload

--- Week-2/Assignment-2/FunctionArrayAndObject.js
+++ Week-2/Assignment-2/FunctionArrayAndObject.js
@@ -1,1 +1,38 @@
+/*
+Assignment 2: Function, Array, and Object
+Complete the function below to calculate the average price of all the products.
+*/
 
+function avg(data) {
+    // your code here
+    const productInfo = data.products;
+    let sumPrice = 0;
+    productInfo.forEach( product => {
+        sumPrice += product.price;
+    });
+
+const avgPrice = sumPrice / productInfo.length;
+return avgPrice;
+}        
+
+
+    
+    console.log(
+        avg({
+        size: 3,
+        products: [
+        {
+        name: 'Product 1',
+        price: 100,
+        },
+        {
+        name: 'Product 2',
+        price: 700,
+        },
+        {
+        name: 'Product 3',
+        price: 250,
+        },
+        ],
+        })
+        ); // should print the average price of all products
